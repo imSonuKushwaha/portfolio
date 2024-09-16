20d640ac0a8d2af60b360e041aef36798893693d
Sonu Kushwaha <imsonukushwaha@gmail.com>
2024-09-16 15:05:19 +0545

    ..

--- src/component/Projects/ImageSynthesis.jsx
+++ src/component/Projects/ImageSynthesis.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const ImageSynthesis = () => {
   return (
     <>
-      <div className="w-full flex flex-col justify-center items-start gap-3 border-8 border-gray-600 rounded-3xl p-3 transition duration-500 ease-in-out hover:scale-110 hover:shadow-lg shadow-gray-800 hover:z-10 ">
+      <div className="w-full flex flex-col justify-center items-start gap-3 border-4 border-gray-600 rounded-3xl p-3 transition duration-500 ease-in-out hover:scale-110 hover:shadow-lg shadow-gray-800 hover:z-10 bg-[#44494F]">
         <a
           href="https://ieeexplore.ieee.org/document/10263208"
           target="_blank"
